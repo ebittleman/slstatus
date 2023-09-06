902dedbc178442e349da97f1e24aa067f870e7af
Bittles <1635274+ebittleman@users.noreply.github.com>
2023-09-06 19:44:59 -0400

    Initial Commit

--- config.def.h
+++ config.def.h
@@ -65,5 +65,13 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+    { battery_perc, "[ Battery %s%% ] ", "BAT0"},
+    { netspeed_rx, " [ DL %s] ", "wlp2s0"},
+    { netspeed_tx, " [ UL %s] ", "wlp2s0"},
+    { cpu_perc,     " [ CPU %s%% ] ",     NULL},
+    { ram_perc,     " [ RAM %s%% ] ",     NULL},
+    { run_command,
+      " [ Vol %s ] ",
+      "amixer sget Master | awk -F\"[][]\" '/Left:/ { print $2 }' | head -n1"},
+    { datetime,     "%s",                 "%F %T" },
 };
